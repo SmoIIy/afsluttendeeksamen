cf3bfcdc5d3e6faf89f84f7d4e966192b1b4c9ab
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-11-25 15:31:41 +0100

    Noget

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,100 +1,52 @@
 'use client';
 
-import { createClient } from '@supabase/supabase-js';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+import { Field, Form, Formik } from 'formik';
 
-export default function Form() {
-  const supabase = createClient();
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      date: '',
-    },
-    validationSchema: Yup.object({
-      firstName: Yup.string()
-        .max(15, 'Must be 15 characters or less')
-        .required('Required'),
-      lastName: Yup.string()
-        .max(20, 'Must be 20 characters or less')
-        .required('Required'),
-      email: Yup.string().email('Invalid email address').required('Required'),
-      date: Yup.date('Venligst vælg dato').required('Required'),
-    }),
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+import { supabase } from '../utils/supabase/settings';
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+async function handleForm(value) {
+  alert(JSON.stringify(value, null, 2));
+
+  const { data, error } = await supabase
+    .from('test')
+    .insert([{ some_column: 'someValue', other_column: 'otherValue' }])
+    .select();
+}
+
+export default function customerForm() {
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label className="label" htmlFor="firstName">
-        First Name
-      </label>
-      <input
-        className="input"
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstName}
-      />
-      {formik.touched.firstName && formik.errors.firstName ? (
-        <div className="label">{formik.errors.firstName}</div>
-      ) : null}
+    <div>
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        onSubmit={async (values) => {
+          await sleep(500);
+          handleForm(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <label htmlFor="firstName">First Name</label>
+            <Field name="firstName" placeholder="Jane" />
 
-      <label className="label" htmlFor="lastName">
-        Last Name
-      </label>
-      <input
-        className="input"
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
-      />
-      {formik.touched.lastName && formik.errors.lastName ? (
-        <div className="label">{formik.errors.lastName}</div>
-      ) : null}
+            <label htmlFor="lastName">Last Name</label>
+            <Field name="lastName" placeholder="Doe" />
 
-      <label className="label" htmlFor="email">
-        Email Address
-      </label>
-      <input
-        className="input"
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email ? (
-        <div className="label">{formik.errors.email}</div>
-      ) : null}
-      <label className="label" htmlFor="date">
-        Date
-      </label>
-      <input
-        className="input"
-        id="date"
-        name="date"
-        type="date"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.date}
-      />
-      {formik.touched.date && formik.errors.date ? (
-        <div className="label">{formik.errors.date}</div>
-      ) : null}
+            <label htmlFor="email">Email</label>
+            <Field name="email" placeholder="jane@acme.com" type="email" />
 
-      <button className="button" type="submit">
-        Submit
-      </button>
-    </form>
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
