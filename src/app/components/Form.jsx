30d0a4dd4df5fd33a4912570a8bf638de265cba4
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-11-26 13:06:36 +0100

    Base database setup form

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,52 +1,67 @@
-'use client';
+import { supabase } from "../utils/supabase/settings";
 
-import { Field, Form, Formik } from 'formik';
+async function handleForm(formdata) {
+	"use server";
 
-import { supabase } from '../utils/supabase/settings';
+	const formData = [
+		{
+			date: "2017-06-01",
+			firstname: "Janus",
+			lastname: "Bardrum",
+			email: "test@test.dk",
+		},
+	];
 
-const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+	const { data, error } = await supabase
+		.from("test")
+		.insert(formData)
+		.select();
 
-async function handleForm(value) {
-  alert(JSON.stringify(value, null, 2));
-
-  const { data, error } = await supabase
-    .from('test')
-    .insert([{ some_column: 'someValue', other_column: 'otherValue' }])
-    .select();
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(data);
+	}
 }
 
-export default function customerForm() {
-  return (
-    <div>
-      <h1>Sign Up</h1>
-      <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          email: '',
-        }}
-        onSubmit={async (values) => {
-          await sleep(500);
-          handleForm(values);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <label htmlFor="firstName">First Name</label>
-            <Field name="firstName" placeholder="Jane" />
-
-            <label htmlFor="lastName">Last Name</label>
-            <Field name="lastName" placeholder="Doe" />
-
-            <label htmlFor="email">Email</label>
-            <Field name="email" placeholder="jane@acme.com" type="email" />
-
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
+export default async function customerForm() {
+	return (
+		<form action={handleForm}>
+			<div>
+				<label className="label" htmlFor="firstname">
+					Fornavn
+				</label>
+				<input
+					className="input"
+					type="text"
+					name="firstname"
+					id="firstname"
+				/>
+			</div>
+			<div>
+				<label className="label" htmlFor="lastname">
+					Efternavn
+				</label>
+				<input
+					className="input"
+					type="text"
+					name="lastname"
+					id="lastname"
+				/>
+			</div>
+			<div>
+				<label className="label" htmlFor="email">
+					Email
+				</label>
+				<input className="input" type="email" name="email" id="email" />
+			</div>
+			<div>
+				<label className="label" htmlFor="date">
+					Dato
+				</label>
+				<input className="input" type="date" name="date" id="date" />
+			</div>
+			<input className="button" type="submit" value="Send" />
+		</form>
+	);
 }
