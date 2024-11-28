4dd1016dbdd0c43468e2384c004158a4b0154661
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-11-28 13:36:02 +0100

    Oprettet buttons

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -2,13 +2,7 @@ import cn from "classnames";
 
 export default async function Button({ anchor, body, className }) {
 	return (
-		<a
-			className={cn(
-				className,
-				"px-8 py-1 border-4 text-lg rounded-tl-2xl rounded-br-2xl transition-all",
-			)}
-			href={anchor}
-		>
+		<a className={cn(className, "")} href={anchor}>
 			{body}
 		</a>
 	);
