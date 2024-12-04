bc557cd65ffe8cfb9f2d7ec357cec68e32026543
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-12-04 19:48:26 +0100

    Putting Header and footer to OM

--- src/app/om/page.jsx
+++ src/app/om/page.jsx
@@ -1,5 +1,12 @@
 import Mobilemenu from "../components/Mobilemenu";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default async function Om() {
-	return <Mobilemenu />;
+	return (
+		<div>
+			<Header />
+			<Footer />
+		</div>
+	);
 }
