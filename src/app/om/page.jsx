f36c81b5ad18922072ea724f7a5b55fcbb0af673
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-12-04 19:56:48 +0100

    Update page.jsx

--- src/app/om/page.jsx
+++ src/app/om/page.jsx
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 
 export default async function Om() {
 	return (
-		<div className="min-h-screen">
+		<div className="min-h-screen bg-[url('/bg_om.jpg')] bg-no-repeat bg-cover">
 			<Header />
 			<main>H</main>
 			<Footer />
