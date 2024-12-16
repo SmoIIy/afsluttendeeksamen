bd147a487ac1ed6a1d9516689f41354cd83808ba
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-12-16 17:24:43 +0100

    Update Hvemerper.jsx

--- src/app/components/Hvemerper.jsx
+++ src/app/components/Hvemerper.jsx
@@ -7,12 +7,17 @@ export default async function Hvemerper({ className }) {
 		<section className={cn(className, "max-w-6xl mx-auto p-4 lg:p-0")}>
 			<h1 className="mb-4">Hvem er DJ Per?</h1>
 			<p className="mb-4 max-w-prose">
-				Sammen kan vi få gang i festen og skabe den helt rette
-				atmosfære. Uanset om det er et bryllup, en firmafest eller en
-				privat fest, er vi klar til at skræddersy musikken, så din
-				begivenhed bliver uforglemmelig.
+				Jeg er DJ Per, og jeg brænder for at skabe øjeblikke, hvor
+				dansegulvet er fyldt, og stemningen er helt i top. Med en
+				kærlighed til musik og erfaring fra mange forskellige events
+				sørger jeg for, at musikken altid passer perfekt til festen og
+				giver dig og dine gæster en oplevelse, I sent vil glemme.
 			</p>
-			<Image src={perportræt} alt="Billede af per" className="w-full" />
+			<Image
+				src={perportræt}
+				alt="Billede af per"
+				className="w-full max-h-[400px] object-cover"
+			/>
 		</section>
 	);
 }
